432f220a96ac40df7ad7332ccae1af6627b4c127
Raymond Hunce <hunce18c@gmail.com>
2021-02-19 11:26:30 -0500

    Make grammatical corrections

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Simple GraphQL Server/Playground
 
-This repository consists of a simple GraphQL server with a couple local variables (owners and pets) acting as our database, for simplicity's sake. The purpose of this is to give anyone an already constructed GraphQL server to look through, play around with, and become familiar through. There is a fair amount of psuedocode within the server.js file in the repository explaining the parts of the code, as well as explaining how to use GraphQL's graphiql tool in your browser (analogous to Postman for a RESTful API) to make requests to the server and see what data you get back.
+This repository consists of a simple GraphQL server with a couple local variables (owners and pets) acting as our database, for simplicity's sake. The purpose of this is to give anyone an already constructed GraphQL server to look through, play around with, and become familiar with. There is a fair amount of psuedocode within the server.js file in the repository explaining the parts of the code, as well as explaining how to use GraphQL's graphiql tool in your browser (analogous to Postman for a RESTful API) to make requests to the server and see what data you get back.
 
 ## Getting Started
 
@@ -17,7 +17,7 @@ npm install
 ```
 npm run start
 ```
-(5) Look through the server.js and read the psuedocode to become familiar with a simple GraphQL server and how it's constructed.
+(5) Look through the server.js file and read the psuedocode to become familiar with a simple GraphQL server and how it's constructed.
 
 (6) Once done, type
 ```
@@ -25,9 +25,9 @@ localhost:8080/graphql
 ```
 into your browser and hit ENTER. The GraphiQL tool should open up.
 
-(7) Use the example queries that are commented out at the bottom of the server.js file in the GraphiQL tool in your browser to make queries, mutations, see what data comes back, etc...
+(7) Use the example queries that are commented out at the bottom of the server.js file in the GraphiQL tool in your browser to test out queries, mutations, see what data comes back, etc...
 
-(8) Also, toward the top-right of the GraphiQL tool in your browser, there's a Docs tab. Click it and play around with it. It shows you, for the server, what root types exist (i.e. query and mutation, in our case), etc... This is especially useful for anyone useing a GraphQL server they didn't set up, as it informs them what kinds of queries can be made and what information is available.
+(8) Also, toward the top-right corner of the GraphiQL tool in your browser, there's a Docs tab. Click it and play around with it. It shows you, for the server, what root types exist (i.e. query and mutation, in our case), etc... This is especially useful for anyone using a GraphQL server they didn't set up, as it informs them what kinds of queries can be made and what information is available.
 
 From here, you can play around with the queries/mutations, and even play around with the code in sevrer.js.
 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLList
 const app = express();
 
 // OUR "DATABASE"
-// The "owners" and "pets" variables are storing our data locally, so as to avoid having to set up a database for this simple server. Notice the relationship between "owners" and "pets," where each pet has an owner indicated by the ownerId in each element of the pets array. I will refer to each of these variables as 'collections' (i.e. the pets collection and owners collection).
+// The "owners" and "pets" variables below are storing our data locally, so as to avoid having to set up a database for this simple server. Notice the relationship between "owners" and "pets," where each pet has an owner indicated by the ownerId. I will refer to each of these variables as 'collections' (i.e. the pets collection and owners collection).
 const owners = [
   { id: 1, name: 'Joey Calamad' },
   { id: 2, name: 'Johnny Basanagol' },
@@ -60,9 +60,10 @@ const OwnerType = new GraphQLObjectType({
   })
 })
 
-// NOW THAT WE'RE DONE SETTING UP EACH "TYPE" FOR EACH COLLECTION, WE CAN SET UP THE KINDS OF REQUESTS WE CAN MAKE TO OUR SERVER. ONE KIND OF REQUEST BELOW WILL BE A "QUERY," WHICH FUNCTIONS LIKE A GET REQUEST (IN A REST API), GRABBING AND RETURNING DATA FROM THE DATABASE FOR YOU. THE OTHER WE WILL SET UP WILL BE A "MUTATION," WHICH FUNCTIONS LIKE A POST REQUEST (IN A REST API), ADDING DATA TO THE DATABASE FOR YOU (IN OUR CASE, THE "DATABASE" CONSISTS OF THE OWNERS AND PETS VARIABLES ABOVE).
+// NOW THAT WE'RE DONE SETTING UP EACH "TYPE" FOR EACH COLLECTION, WE CAN SET UP THE KINDS OF REQUESTS WE CAN MAKE TO OUR SERVER. ONE KIND OF REQUEST BELOW WILL BE A "QUERY," WHICH FUNCTIONS LIKE A GET REQUEST (IN A REST API), GRABBING AND RETURNING DATA FROM THE DATABASE. THE OTHER KIND OF REQUEST WE WILL SET UP WILL BE A "MUTATION," WHICH FUNCTIONS LIKE A POST REQUEST (IN A REST API), ADDING DATA TO THE DATABASE FOR YOU.
+// IN OUR CASE, THE "DATABASE" CONSISTS OF THE OWNERS AND PETS VARIABLES ABOVE.
 
-// This allows the frontend to make queries to our server to retrieve data from the database. If you look within the fields method, it indicates we could query for a single pet (and the args indicates we'll have to provide an id for the pet), we could query for all the pets, we could query for a single owner (and the args indicates we'll have to provide an id for the owner), and we could query for all the owners. Queries can also consists of a mixture of these (i.e. can query for all the owners and for each of them, show all the pets). Some of the query examples at the bottom of this file demonstrate this.
+// This allows the frontend to make queries to our server to retrieve data from the database (like a GET request). If you look within the fields method, it indicates we could query for a single pet (and the args indicates we'll have to provide an id for the pet), we could query for all the pets, we could query for a single owner (and the args indicates we'll have to provide an id for the owner), and we could query for all the owners. Queries can also consist of a mixture of these things (e.g. can query for all the owners and for each of them, show all their pets). Some of the query examples at the bottom of this file demonstrate this.
 const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root Query',
@@ -96,7 +97,7 @@ const RootQueryType = new GraphQLObjectType({
   }),
 });
 
-// This allows the frontend to make "mutations" to our server to add data to our database. If you look within the fields method, it indicates we could add a new pet (and the args indicates we'll have to provide a name and ownerId for the newly created pet), and we can add a new owner (and the args indicates we'll have to provide a name for the newly created owner).
+// This allows the frontend to make "mutation" requests to our server to add data to our database (like a POST request). If you look within the fields method, it indicates we could add a new pet (and the args indicates we'll have to provide a name and ownerId for the newly created pet), and we can add a new owner (and the args indicates we'll have to provide a name for the newly created owner).
 const RootMutationType = new GraphQLObjectType({
   name: 'Mutation',
   description: 'Root Mutation',
@@ -136,25 +137,28 @@ const RootMutationType = new GraphQLObjectType({
   })
 })
 
+// WITH THE ABOVE REQUEST TYPES FINISHED...
+
 // We create our schema here, as so
 const schema = new GraphQLSchema({
   query: RootQueryType,
   mutation: RootMutationType
 });
 
-// we set up an endpoint for our requests ...
+// We set up an endpoint for our requests (GraphQL is characterized by having only one endpoint, and the data you get not being a function of having multiple endpoints, but rather being dictated by the query/queries you formulate in the front-end, so you can get the data you want, nothing more and nothing less)...
 app.use('/graphql', graphqlHTTP({
   schema: schema,
-  graphiql: true // allows us to use the GraphiQL tool (analogous to Postman, except for GraphQL) to manually issue GraphQL queries
+  graphiql: true // allows us to use the GraphiQL tool (analogous to Postman, except for GraphQL) to manually issue GraphQL queries. This tool comes up when you go to this endpoint in your browser.
 }));
 
-// ... and listen for it on port 8080 in this case
+// ... and listen on port 8080 in this case
 app.listen(8080, () => {
   console.log('Running a GraphQL API server at http://localhost:8080/graphql');
 });
 
-// AT THIS POINT YOU CAN START YOUR SERVER ('npm install' and then 'npm run start' in your terminal from this repo's root directory, if you haven't done this already), THEN TYPE localhost:8080/graphql INTO YOUR BROWSER, HIT ENTER, AND THE GRAPHIQL TOOL (a tool like Postman, allowing you to make HTTP requests without having a built-out frontend) WILL POP UP IN YOUR BROWSER, WHERE YOU CAN TRY OUT VARIOUS QUERIES AND MUTATIONS AND SEE WHAT DATA YOU GET BACK FROM THIS SERVER.
-// YOU CAN USE THE QUERIES/MUTATIONS BELOW IN THE GRAPHIQL TOOL IN YOUR BROWSER. YOU CAN ONLY RUN ONE QUERY/MUTATION AT A TIME, SO MAKE SURE THE OTHERS ARE COMMENTED OUT BEFORE YOU HIT THE RUN BUTTON IN THE GRAPHIQL TOOL (in the event you copy and paste all of the below in the tool at once, which is fine). FROM THERE YOU CAN JUST PLAY AROUND WITH THE QUERIES/MUTATIONS, AND THEN MAYBE EVEN PLAY AROUND WITH THE ABOVE CODE TO MODIFY THE SERVER AND THUS THE QUERIES/MUTATIONS YOU CAN MAKE.
+// AT THIS POINT YOU CAN START YOUR SERVER ('npm install' and then 'npm run start' in your terminal from this repo's root directory if you haven't done so already), THEN TYPE localhost:8080/graphql INTO YOUR BROWSER, HIT ENTER, AND THE GRAPHIQL TOOL (a tool like Postman, allowing you to make HTTP requests without having a built-out frontend) WILL POP UP IN YOUR BROWSER, WHERE YOU CAN TRY OUT VARIOUS QUERIES AND MUTATIONS AND SEE WHAT DATA YOU GET BACK FROM THIS SERVER.
+
+// YOU CAN USE THE QUERIES/MUTATIONS BELOW IN THE GRAPHIQL TOOL IN YOUR BROWSER TO GET YOU STARTED. YOU CAN ONLY RUN ONE QUERY/MUTATION AT A TIME, SO MAKE SURE THE OTHERS ARE COMMENTED OUT BEFORE YOU HIT THE RUN BUTTON IN THE GRAPHIQL TOOL (in the event you copy and paste all of the below in the tool at once, which is fine). FROM THERE YOU CAN JUST PLAY AROUND WITH THE QUERIES/MUTATIONS, AND THEN MAYBE EVEN PLAY AROUND WITH THE ABOVE CODE TO MODIFY THE SERVER AND THUS THE QUERIES/MUTATIONS YOU CAN MAKE.
 
 // LASTLY IN THE GRAPHIQL TOOL IN YOUR BROWSER, IN THE TOP RIGHT IS A "DOCS" TAB. CLICK ON THAT AND PLAY AROUND WITH IT. IT SHOWS YOU, FOR YOUR SERVER, WHAT ROOT TYPES EXIST, WHAT FIELDS EXISTS IN THOSE, WHAT FIELDS EXISTS WITHIN THOSE FIELDS, ETC..., INFORMING THE KINDS OF QUERIES THAT ARE POSSIBLE AND WHAT INFORMATION EXISTS. THIS IS ESPECIALLY USEFUL IF YOU'RE USING A GRAPHQL SERVER THAT YOU DIDN'T CREATE AND ARE THEREFORE NOT FAMILIAR WITH.
 
